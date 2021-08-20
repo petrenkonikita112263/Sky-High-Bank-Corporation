ea6a10d12fe810961eaaa924693eea4cf9deed8f
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-08-20 10:09:02 +0300

    Rewrite SavingAccount class, which now implements the interface not extends the class.

--- src/dev/app/bank/SavingAccount.java
+++ src/dev/app/bank/SavingAccount.java
@@ -1,45 +1,18 @@
 package dev.app.bank;
 
-import static dev.app.bank.BankConfiguration.BANK_RATE;
-
 /**
  * Saving account, which balance can be increased by add interest method.
  * Has account number, balance and type of account - foreign or domestic.
  *
  * @author Nikita Petrenko
  */
-public class SavingAccount extends AbstractBankAccount {
-
-    /**
-     * EVC.
-     *
-     * @param accountNumber owner account number
-     */
-    public SavingAccount(int accountNumber) {
-        super(accountNumber);
-    }
+public class SavingAccount implements AccountTypeStrategy {
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public void addInterest() {
-        balance += (int) (balance * (1 + BANK_RATE));
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean hasEnoughMoney(int amountLoan) {
-        return balance >= amountLoan / 2;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected double collateralRatio() {
+    public double collateralRatio() {
         return 1.0 / 2.0;
     }
 
@@ -47,15 +20,15 @@ public class SavingAccount extends AbstractBankAccount {
      * {@inheritDoc}
      */
     @Override
-    protected double interestRate() {
+    public double interestRate() {
         return 0.01;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public String toString() {
-        return "Saving account "
-                + accountNumber + ": balance = " + balance
-                + (owner.isForeign() ? ", and it's foreign account"
-                : ", and it's domestic account");
+    public String accountType() {
+        return "Saving account";
     }
 }
