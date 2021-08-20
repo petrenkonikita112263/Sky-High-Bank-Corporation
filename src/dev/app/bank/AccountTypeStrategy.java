97bee4bc5fe29e2d8edd2652c49c53bf8500dbc0
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-08-20 10:05:54 +0300

    Created new strategy interface.
    AccountTypeStrategy is the interface with three methods collateralRatio, interestRate and accountType.

---
+++ src/dev/app/bank/AccountTypeStrategy.java
@@ -0,0 +1,31 @@
+package dev.app.bank;
+
+/**
+ * Strategy interface class that declares some methods.
+ *
+ * @author Nikita Petrenko
+ */
+public interface AccountTypeStrategy {
+
+    /**
+     * Helper method for hasEnoughMoney.
+     *
+     * @return value of ratio depending on account type
+     */
+    double collateralRatio();
+
+    /**
+     * Helper method for addInterest method.
+     *
+     * @return value of rate depending on account type
+     */
+    double interestRate();
+
+    /**
+     * Helper method for printing the type of account.
+     * (saving, interest or checking)
+     *
+     * @return type of bank account
+     */
+    String accountType();
+}
