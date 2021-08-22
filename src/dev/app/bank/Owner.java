e65550893f772fca1d500c7dd72c831982b81a82
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-08-22 21:55:12 +0300

    Updated Owner.java.
    Added constant values, three fields to the enum class, created constructor and overrides methods
    with new implemintation.

--- src/dev/app/bank/Owner.java
+++ src/dev/app/bank/Owner.java
@@ -1,15 +1,31 @@
 package dev.app.bank;
 
 public enum Owner implements OwnerStrategy{
-    ;
+    DOMESTIC(false, 0, "domestic"),
+    FOREIGN(true, 5, "foreign");
+
+    private boolean isForeign;
+    private int feeAmount;
+    private String ownerName;
+
+    Owner(boolean isForeign, int feeAmount, String ownerName) {
+        this.isForeign = isForeign;
+        this.feeAmount = feeAmount;
+        this.ownerName = ownerName;
+    }
 
     @Override
     public boolean isForeign() {
-        return false;
+        return isForeign;
     }
 
     @Override
     public int fee() {
-        return 0;
+        return feeAmount;
+    }
+
+    @Override
+    public String toString() {
+        return ownerName;
     }
 }
