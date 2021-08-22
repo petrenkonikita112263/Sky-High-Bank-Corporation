e9ac8cc905bf084fde6dd4ad5a02824bd35b3fbb
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-08-22 22:21:09 +0300

    Updated CommandsList.java.
    Filled enum class with constants. These contants hold command name and it's option: what operation
    will be execute. This thing is represented by two fields and constructor. Added new implemention to
    overriden method.

--- src/dev/app/bank/commands/CommandsList.java
+++ src/dev/app/bank/commands/CommandsList.java
@@ -2,13 +2,89 @@ package dev.app.bank.commands;
 
 import dev.app.bank.Bank;
 
+import java.io.InputStream;
 import java.util.Scanner;
 
-public enum CommandsList implements InputCommand{
+public enum CommandsList implements InputCommand {
+    QUIT("quit", (scanner, bank, current) ->
+    {
+        System.out.println("Thanks for using us! Have a nice day!");
+        return -1;
+    }),
+    CREATE("create", (scanner, bank, current) ->
+    {
+        System.out.println("Enter account type(1=savings,"
+                + " 2=checking, 3=interest checking): ");
+        int type = scanner.nextInt();
+        System.out.print("Enter 1 for foreign, 2 for domestic: ");
+        int valBoolean = scanner.nextInt();
+        boolean isForeign = (valBoolean == 1);
+        current = bank.newAccount(type, isForeign);
+        System.out.println("Your new account number is "
+                + current);
+        return current;
+    }),
+    SELECT("select", (scanner, bank, current) ->
+    {
+        System.out.print("Enter the bank account number: ");
+        current = scanner.nextInt();
+        int balance = bank.getBalance(current);
+        System.out.println("The balance of account "
+                + current + " is " + balance);
+        return current;
+    }),
+    DEPOSIT("deposit", (scanner, bank, current) ->
+    {
+        System.out.print("Enter deposit amt: ");
+        int amount = scanner.nextInt();
+        bank.deposit(current, amount);
+        return current;
+    }),
+    LOAN("loan", (scanner, bank, current) ->
+    {
+        System.out.print("Enter loan amount: ");
+        int amountLoan = scanner.nextInt();
+        if (bank.authorizeLoan(current, amountLoan))
+            System.out.println("Your loan is approved");
+        else
+            System.out.println("Your loan is denied");
+        return current;
+    }),
+    SHOW("show", (scanner, bank, current) ->
+    {
+        System.out.println(bank.toString());
+        return current;
+    }),
+    INTEREST("interest", (scanner, bank, current) ->
+    {
+        bank.addInterest();
+        return current;
+    }),
+    SET_FOREIGN_STATE("set foreign status", (scanner, bank, current) ->
+    {
+        System.out.print("Enter 1 for foreign, 2 for domestic: ");
+        int valBoolean = scanner.nextInt();
+        boolean isForeign = (valBoolean == 1);
+        bank.setForeign(current, isForeign);
+        return current;
+    }),
     ;
+
+    private String commandName;
+    private InputCommand commandOption;
+
+    CommandsList(String commandName, InputCommand commandOption) {
+        this.commandName = commandName;
+        this.commandOption = commandOption;
+    }
 
     @Override
     public int execute(Scanner scanner, Bank bank, int current) {
-        return 0;
+        return commandOption.execute(scanner, bank, current);
+    }
+
+    @Override
+    public String toString() {
+        return commandName;
     }
 }
